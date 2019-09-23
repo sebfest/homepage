93af541a941666a4fd3d10195a987f43463fc4e0
sebfest <sebastian.fest@gmail.com>
2019-09-23 09:40:19 +0200

    new css

--- homepage_files/homepage/fixtures.py
+++ homepage_files/homepage/fixtures.py
@@ -1,7 +1,9 @@
 import os
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'homepage.settings')
 
 import django
+
 django.setup()
 
 from blog.models import Post
@@ -19,3 +21,28 @@ def populate():
 if __name__ == '__main__':
     print("Starting blog population script...")
     populate()
+
+logging_config = dict(
+    version=1,
+    formatters={
+        'general': {'format': '%(asctime)s %(levelname)s %(message)s'}
+    },
+    handlers={
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'general',
+            'level': 'DEBUG'
+        },
+        'file': {
+            'class': 'logging.FileHandler',
+            'formatter': 'general',
+            'level': 'DEBUG',
+            'filename': 'logs/logfile.log',
+            'mode': 'a',
+        }
+    },
+    root={
+        'handlers': ['console', 'file'],
+        'level': 'INFO',
+    },
+)
