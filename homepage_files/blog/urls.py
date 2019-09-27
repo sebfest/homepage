6722619056b589f9a3b1049ed8645dbc015a8afb
sebfest <sebastian.fest@gmail.com>
2019-09-27 10:48:02 +0200

    Added RSS feed

--- homepage_files/blog/urls.py
+++ homepage_files/blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from blog import views
+from blog import views, feed
 
 app_name = 'blog'
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
     path('tag/<slug:slug>/', views.PostTagListView.as_view(), name='post_tag_list'),
     path('post/<slug:slug>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('feed/', feed.PostFeed(), name='post_feed'),
 ]
