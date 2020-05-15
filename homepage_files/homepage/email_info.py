dc023a16d61023aaf05d1187286d581616de8337
sebfest <sebastian.fest@gmail.com>
2020-05-15 23:28:44 +0200

    Reformated allignment

--- homepage_files/homepage/email_info.py
+++ homepage_files/homepage/email_info.py
@@ -2,6 +2,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'some.mail@gmail.com'
+EMAIL_HOST_USER = 'sebastian.fest@gmail.com'
 EMAIL_HOST_PASSWORD = 'test'
 DEFAULT_FROM_EMAIL = True
